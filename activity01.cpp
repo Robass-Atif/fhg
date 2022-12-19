4c4a2179a79f72969a45c455553ccf7a1bade7a7
robass atif <mohammadrobass@gmail.com>
2022-12-18 21:51:21 -0800

    message

---
+++ activity01.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+main() {
+cout << "                    ---------------   "<<endl;
+cout <<"                   </   0       0   \\>"<<endl;
+cout <<"                   I                 I	 "<<endl;
+cout <<"                  /I        !        I\\"<<endl;
+cout <<"                 / I                 I \\"<<endl;
+cout <<"                /  I                 I  \\"<<endl;
+cout <<"               /   I        ~        I   \\"<<endl;
+cout <<"            ( )    I                 I   (  )"<<endl;
+cout <<"                  \\                 /"<<endl;
+cout << "                    ---------------"<<endl;
+cout <<"                       I         I          "<<endl;      
+cout <<"                       I         I"<<endl;
+cout <<"                       I         I"<<endl;
+cout <<"                       I         I"<<endl;
+cout <<"                   @_@_I         I _@_@__"<<endl;
+
+
+
+
+
+
+}
---
+++ robass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+
+cout << "this is my first programming fundamental class" << endl;
+cout << "my name is robass" << endl;
+cout <<  "my roll no. is 114118" << endl;
+cout << "I am sitting in n-12" << endl;
+cout << " I am  working on notepad" << endl;
+cout << "My section is C" << endl;
+}
